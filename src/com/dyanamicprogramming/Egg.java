0370b8b4d9a87e8f837e58bb001e2dd92ea6cc6a
Your Name <you@example.com>
2024-06-11 10:47:33 +0530

    egg drop problem

--- src/com/dyanamicprogramming/Egg.java
+++ src/com/dyanamicprogramming/Egg.java
@@ -1,18 +1,48 @@
 package com.dyanamicprogramming;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Egg {
-	
-	public int eggDrop(int n, int k) {
-		int floor = k;
-		int eggLeft = n;
-		int count = 0;
-		while(floor>0 && eggLeft>1) {
-			floor = floor/2;
-			eggLeft--;
-			count++;
+
+	public Integer eggDrop(int n, int k) {
+
+		if (k == 1) {
+			return 1;
 		}
-		
-		return count + floor;
+		if (n == 1) { 
+			return k;
+		}
+
+		Integer[][] table = new Integer[n + 1][k + 1];
+
+		for (int i = 1; i < table.length; i++) {
+			table[i][0] = 0;
+			table[i][1] = 1;
+		}
+		for(int i=0; i<table[1].length;i++) {
+			table[1][i] = i;
+		}
+	
+		for(int i=2;i<table.length;i++) { 
+			for(int j=2;j<table[i].length;j++) {
+				int u = 0;
+				int l = j-1;
+				int min = Integer.MAX_VALUE;
+				while(u<j && l>=0) {
+					int val1 = table[i-1][u];
+					int val2 = table[i][l];
+					int max = Math.max(val1, val2);
+					min = Math.min(max, min);
+					u++;
+					l--;
+				}
+				table[i][j] = min+1;
+				
+			}
+		}
+		return table[n][k];
 	}
 
 }
--- src/com/dyanamicprogramming/Test.java
+++ src/com/dyanamicprogramming/Test.java
@@ -9,13 +9,10 @@ public class Test {
 	
 public static void main(String[] args) {
 	
-	Coin c = new Coin();
-	
-	int arr[] = new int[] {2};
-	
-	System.out.println(c.minimumCoin(arr, 3));;
+	Egg e = new Egg();
+	System.out.println(e.eggDrop(4, 3));
 
-
+ 
 
 }
 }
